695352e6f418c71b8f4fc8a9b806815cec93a771
Syoker <lautarivan@hotmail.com>
2022-08-29 18:57:16 -0300

    add optional install

--- install.sh
+++ install.sh
@@ -6,6 +6,42 @@ LATESTARTSERVICE=false
 REPLACE="
 "
 
+android_check() {
+ if (( $API < 30 )); then
+    ui_print "• Sorry, support for Android 11 only."
+    ui_print ""
+    sleep 2
+    exit 1
+ fi
+ if (( $API > 30 )); then
+    ui_print "• Sorry, support for Android 11 only."
+    ui_print ""
+    sleep 2
+    exit 1
+  fi
+}
+
+volume_keytest() {
+  ui_print "• Volume Key Test"
+  ui_print "  Please press any key volume:"
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events) || return 1
+  return 0
+}
+
+volume_key() {
+  while (true); do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events
+      if $(cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null); then
+          break
+      fi
+  done
+  if $(cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null); then
+      return 1
+  else
+      return 0
+  fi
+}
+
 print_modname() {
   ui_print ""
   ui_print "•••••••••••••••••••••••••••••"
@@ -19,8 +55,42 @@ print_modname() {
 }
 
 on_install() {
-  ui_print "- Extracting module files"
+
+  android_check
+
+  ui_print "• Extracting module files"
+  ui_print ""
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+
+  if volume_keytest; then
+    ui_print "  Key test function complete"
+    ui_print ""
+    sleep 2
+
+    ui_print "• Do you want to apply changes to the launcher?"
+    ui_print "  Volume up(+): Yes"
+    ui_print "  Volume down(-): No"
+
+    SELECT=volume_key
+    
+    if "$SELECT"; then
+      ui_print "  Removing changes to the launcher"
+      ui_print ""
+      rm $MODPATH/system/system_ext/priv-app/Launcher3QuickStep/Launcher3QuickStep.apk
+      sleep 2
+    else
+      ui_print "  Applying changes to the launcher"
+      ui_print ""
+      sleep 2
+    fi
+
+  else
+    ui_print "  You have not pressed any key, aborting installation."
+    ui_print ""
+    sleep 2
+    exit 1
+  fi
+  
   ui_print "- Deleting package cache"
   rm -rf /data/system/package_cache/*
 }
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Fix Official Firmware for Astro
+
+## Description
+This module fixes some things in the stock firmware of the Motorola One Fusion.
+
+## Content
+- Rounded corners of applications in their opening animation
+- Increased rounding of some buttons and the notification panel.
+- In the stock launcher, the name of the applications in the drawer will now have only 1 line. (optional)
+
+## Compatibility
+- Android 11
+
+## Notes
+- Only for stock firmware of Motorola One Fusion.
+
+## Tested
+- Android 11 (RPLS31.Q2-63-10-2-9)
